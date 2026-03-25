1ef99845a5d10843b9163529288a7109e6c24bf3
Anis Benabdallah <anis.benabdallah1@icloud.com>
2026-03-25 15:01:41 -0400

    JA : line sensor functions. Need to test

--- tp/lib/lineSensor.cpp
+++ tp/lib/lineSensor.cpp
@@ -1,29 +1,75 @@
-#pragma once
 #define F_CPU 8000000UL
 #include "libstatique.hpp"
 
-#define sensor1 PA0
-#define sensor2 PA1
-#define sensor3 PA2
-#define sensor4 PA3
-#define sensor5 PA4
+
+
+// Ici je prends en compte quon utilise le mode qui detecte les zones sombres et non les zones claires.
 
 LineSensor::LineSensor() {
-    DDRA |= (1 << sensor1) | (1 << sensor2) | (1 << sensor3) | (1 << sensor4) | (1 << sensor5);
+    DDRA |= (1 << PA0) | (1 << PA1) | (1 << PA2) | (1 << PA3) | (1 << PA4);
 };
 
 LineSensor::~LineSensor() {};
 
-bool LineSensor::isMiddle() {
-    
-}
-bool LineSensor::isOffTrackLeft() {
-
-}
-
-bool LineSensor::isOffTrackRight() {
-
+bool LineSensor::robotMiddle() {
+    if (!((PINA & (1 << sensor1)) || (PINA & (1 << sensor2)) || (PINA & (1 << sensor3)) || (PINA & (1 << sensor4)) || (PINA & (1 << sensor5)))) {
+        return true;
+    }
+    return false;
 }
 
+bool LineSensor::robotOffTrackLeft() {
+    if ((PINA & (1 << sensor1)) && !(PINA & (1 << sensor5))) {
+        return true;
+    }
+    return false;
+}
 
+bool LineSensor::robotOffTrackRight() {
+    if (!(PINA & (1 << sensor1)) && (PINA & (1 << sensor5))) {
+        return true;
+    }
+    return false;
+}
+
+bool LineSensor::robotBumpLine() {
+    if ((PINA & (1 << sensor1)) && (PINA & (1 << sensor2)) && (PINA & (1 << sensor3)) && (PINA & (1 << sensor4)) && (PINA & (1 << sensor5))) {
+        return true;
+    }
+    return false;
+}
+
+bool LineSensor::isLeftWall() {
+    if ((PINA & (1 << sensor1)) && (PINA & (1 << sensor2))) {
+        return true;
+    }
+    return false;
+}
+
+bool LineSensor::isRightWall() {
+    if ((PINA & (1 << sensor4)) && (PINA & (1 << sensor5))) {
+        return true;
+    }
+    return false;
+}
+
+bool LineSensor::foundDamage() {
+    if (offTrackAmount() < 3 && PINA & (1 << sensor3)) {
+        return true;
+    }
+    return false;
+}
+
+uint8_t LineSensor::offTrackAmoun
+Nom	Taille
+lineSensor.cpp	1,64 ko
+lineSensor.hppt() {
+    uint8_t amount = 0;
+    for (uint8_t i = 0; i < 5; i++) {
+        if (PINA & (1 << sensors_[i])) {
+            amount++;
+        }
+    }
+    return amount;
+}
 
