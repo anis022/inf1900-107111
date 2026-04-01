c0745b38c4e94365abdb155c1e076813c19bcb24
Anis Benabdallah <anis.benabdallah1@icloud.com>
2026-04-01 19:52:53 -0400

    AB: machine à états (turnleft, etc...)

--- tp/project/main.cpp
+++ tp/project/main.cpp
@@ -1,6 +1,12 @@
 #include "libstatique.hpp"
 #include "lineSensor.hpp"
 
+LineSensor lineSensor;
+Robot robot;
+Timer timer(Timer::TIMER1);
+
+// #define motor robot.motor
+
 enum class State {
     MIDDLE,
     OFF_TRACK_LEFT,
@@ -14,62 +20,127 @@ enum class State {
 enum class Action {
     PARKING,
     FIRST_TURN,
-    SECOND_TURN,
-    END
+    FIRST_CORRIDOR,
+    ENTER_FIRST_ROOM,
+    FIRST_ROOM,
+    END,
 };
+void turnLeft() {
+
+    uint8_t count = lineSensor.offTrackAmount();
+
+    // Virage à gauche proportionnel: plus de capteurs noirs = arc plus serré
+    // Aucun delay → le robot lit les capteurs à chaque itération sans jamais dépasser le tape
+    uint8_t leftSpeed;
+    const uint8_t rightSpeed = 110;
+
+    if      (count == 5) leftSpeed = 0;    // pivot sur place (mur droit devant)
+    else if (count == 4) leftSpeed = 0;
+    else if (count == 3) leftSpeed = 80;
+    else if (count == 2) leftSpeed = 80;
+    else if (count == 1) leftSpeed = 85;
+    else                 leftSpeed = 110;  // aucun capteur → tout droit
+
+    robot.motor.goForward(leftSpeed, rightSpeed);
+}
+
+void followPath() { 
+
+    uint8_t leftWheelSpeed = 100;
+    uint8_t rightWheelSpeed = 100;
+    
+    if (lineSensor.offTrackLeft())
+        rightWheelSpeed += lineSensor.offTrackAmount() * 15;
+    else if (lineSensor.offTrackRight())
+        leftWheelSpeed += lineSensor.offTrackAmount() * 15;
+
+    robot.motor.goForward(leftWheelSpeed, rightWheelSpeed);
+}
+
+void movementLogic(Action& currentAction) {
+    switch (currentAction) {
+
+        case Action::FIRST_TURN:
+            while (!lineSensor.robotMiddle()) {
+                turnLeft();
+                _delay_ms(50);
+            }
+            break;
+
+        case Action::FIRST_CORRIDOR:
+
+            followPath();
+
+            break;
+
+        // case Action::ENTER_FIRST_ROOM:
+        //     robot.motor.goForward(120, 120);
+        //     break;
+        
+        // case Action::FIRST_ROOM:
+        //     robot.motor.goForward(120, 120);
+        //     _delay_ms(1000);
+
+        //     // IMPLEMENTER LOGIQUE
+        //     break;
+            
+        case Action::END:
+// Stop the robot // Turn off LEDs
+            break;
+    }
+}
+
+void switchLogic(Action& currentAction) {
+    switch (currentAction) {
+        // case Action::PARKING:
+        //     if (lineSensor.robotBumpLine()) {
+        //         currentAction = Action::FIRST_TURN;
+        //     }
+        //     break;
+
+        case Action::FIRST_TURN:
+            if (lineSensor.robotMiddle()) {
+                currentAction = Action::FIRST_CORRIDOR;
+            }
+            break;
+
+        case Action::FIRST_CORRIDOR:  
+            if (lineSensor.robotBumpLine()) {
+                currentAction = Action::END; // Transition to the second turn
+            }
+            break;
+
+        // case Action::ENTER_FIRST_ROOM:
+        //     _delay_ms(1000);
+        //     currentAction = Action::FIRST_ROOM;
+
+        case Action::END:
+
+            break;
+    }
+}
+
 
 int main() {
-    LineSensor lineSensor;
-    Robot robot;
-    Action currentAction = Action::PARKING;
+
+    // Action currentAction = Action::PARKING;
+    // while (true) {
+    //     movementLogic(currentAction);
+    //     switchLogic(currentAction);
+    // }
+    // while (true) { 
+    //     while (!lineSensor.robotMiddle()) {
+    //         robot.motor.spinRight(lineSensor.offTrackAmount() * 20); // Example: Spin right at speed 150
+    //         robot.motor.goForward(90, 90); // Move forward at speed 150
+    //         _delay_ms(2000);
+    //     }
+    //     robot.motor.goForward(90, 90); // Move forward at speed 150
+    // }
+    Action currentAction = Action::FIRST_TURN;
+
     while (true) {
-        switch (currentAction) {
-            case Action::PARKING:
-                robot.goBackward(180);
-                _delay_ms(1000);
-                robot.stopMotor();
-                _delay_ms(1000);
-                robot.turnRight(90); // Turn left 180 degrees to face the line
-                robot.stopMotor();
-                _delay_ms(1000);
-                robot.goForward(200); // Move forward at speed 200 until bumping the line
-                _delay_ms(700);
-
-                currentAction = Action::FIRST_TURN; // Transition to the first turn
-                break;
-
-            case Action::FIRST_TURN:
-                // Implement logic for the first turn
-                while (!lineSensor.robotBumpLine()) {
-                }
-                robot.turnLeft(90); // Example: Turn right 90 degrees
-                robot.goForward(200); // Move forward at speed 200
-                // Transition to SECOND_TURN based on conditions
-                if (lineSensor.robotBumpLine()) {
-                    currentAction = Action::SECOND_TURN; // Transition to the second turn
-                }
-                break;
-
-            case Action::SECOND_TURN:
-                // Implement logic for the second turn
-                robot.turnLeft(90); // Example: Turn right 90 degrees
-                robot.goForward(200); // Move forward at speed 200
-                if (lineSensor.foundDamage()) {
-                    robot.playSound(60); // Play a sound to indicate damage found
-                    robot.turnOnGreen(); // Turn on green LED to indicate damage found
-                }
-                // Transition back to PARKING or other states based on conditions
-                if (lineSensor.robotBumpLine()) {
-                    currentAction = Action::END; // Transition to the second turn
-                }
-                break;
-
-            case Action::END:
-                robot.stopMotor(); // Stop the robot
-                robot.turnOffLED(); // Turn off LEDs
-                break;
-        }
+        movementLogic(currentAction);
+        switchLogic(currentAction);
     }
-    
-
 }
+
